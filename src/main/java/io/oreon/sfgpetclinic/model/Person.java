eb32e45ea1b1b6540a7e4aa84f41fb717aae7adf
Robin Hellemans <robin.hellemans@ixortalk.com>
2018-08-30 20:20:28 +0200

    Adding POJOS, closes #2

---
+++ src/main/java/io/oreon/sfgpetclinic/model/Person.java
@@ -0,0 +1,14 @@
+package io.oreon.sfgpetclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/main/java/io/oreon/sfgpetclinic/model/Pet.java
@@ -0,0 +1,12 @@
+package io.oreon.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private LocalDate birthDate;
+
+    private PetType type;
+
+    private Owner owner;
+}
