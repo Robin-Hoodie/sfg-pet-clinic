3e5263f68862d7360ae768bb17af687b65b3c68d
Robin Hellemans <robin.hellemans@ixortalk.com>
2018-09-05 20:39:32 +0200

    Add specialty entity. Add specialty property to vet. Closes #27

---
+++ sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,18 @@
+package io.oreon.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public Specialty(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/model/Vet.java
@@ -1,11 +1,24 @@
 package io.oreon.sfgpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialties = new HashSet<>();
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
     public Vet() {
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
     }
 }
