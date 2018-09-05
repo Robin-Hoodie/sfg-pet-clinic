989f6042eff6bd852c40cf4d34f7ae9329442989
Robin Hellemans <robin.hellemans@ixortalk.com>
2018-09-05 20:43:59 +0200

    Add contact info properties to owner. Closes #33

--- sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/model/Owner.java
@@ -5,13 +5,44 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
+
     private Set<Pet> pets = new HashSet<>();
 
-    public Owner(String firstName, String lastName) {
+    public Owner(String firstName, String lastName, String address, String city, String telephone) {
         super(firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
     }
 
     public Owner() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
