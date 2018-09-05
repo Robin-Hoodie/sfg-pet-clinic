cf86feb09d24f119e1562608f5bdfc9c44aa0022
Robin Hellemans <robin.hellemans@ixortalk.com>
2018-09-05 20:33:43 +0200

    Create Visit Entity, add pets property to Owner. Closes #38

--- sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/model/Owner.java
@@ -1,11 +1,24 @@
 package io.oreon.sfgpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets = new HashSet<>();
 
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
     public Owner() {
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
