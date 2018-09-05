96e969d8dfecdad26fe80a5a85a98515ba62862b
Robin Hellemans <robin.hellemans@ixortalk.com>
2018-09-05 23:20:44 +0200

    Add SpecialtyService & SpecialtyMapService. Closes #45 & closes #44

---
+++ sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/service/SpecialtyService.java
@@ -0,0 +1,6 @@
+package io.oreon.sfgpetclinic.service;
+
+import io.oreon.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ sfg-pet-clinic-data/src/main/java/io/oreon/sfgpetclinic/service/map/SpecialtyMapService.java
@@ -0,0 +1,7 @@
+package io.oreon.sfgpetclinic.service.map;
+
+import io.oreon.sfgpetclinic.model.Specialty;
+import io.oreon.sfgpetclinic.service.SpecialtyService;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+}
